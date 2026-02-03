879d8001d54e463422ad884f50bf3682905a6652
gonzalomarinl <gmarinliduena@gmail.com>
2026-02-03 13:21:02 +0100

    zoom plantas

--- src/sancho_navigation/sancho_navigation/plant_doctor.py
+++ src/sancho_navigation/sancho_navigation/plant_doctor.py
@@ -64,8 +64,8 @@ def analyze_plant(image_path, point_name, mode='sim'):
     h, w, _ = raw_img.shape
     # Definimos un cuadro de 400x400 píxeles en el centro de la imagen
     # Ajustamos y para que capture un poco más abajo (donde suelen estar las hojas)
-    cx, cy = w // 2, h // 2
-    size = 200 # Mitad del tamaño del cuadro (total 400px)
+    cx, cy = w // 2, (h // 2) + 50
+    size = 150 # Mitad del tamaño del cuadro (total 400px)
     crop_img = raw_img[cy-size:cy+size, cx-size:cx+size]
     
     # Si por algún motivo el recorte falla, usamos la imagen original
